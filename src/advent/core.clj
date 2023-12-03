8021b03a4fd588634f8dcde1c35bb0b62571a8a6
Alexander Berezhnoy <alexander@noice.com>
2023-12-03 14:41:22 +0200

    Support --input option

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -9,14 +9,16 @@
          time# (- (. System (currentTimeMillis)) start#)]
      [time# ret#]))
 
-(defn run [year day]
+(defn run [year day input-path]
   (let [ns-name (str "advent.year" year ".day" (format "%02d" day))
         resolve (fn [sym] (find-var (symbol (str ns-name "/" sym))))]
     (require (symbol ns-name))
     (let [parse (resolve "parse")
           solve-1 (resolve "solve-1")
           solve-2 (resolve "solve-2")
-          input (util/load-input year day)
+          input (if (nil? input-path)
+                  (util/load-input year day)
+                  (slurp input-path))
           [tp parsed] (measure (parse input))
           [t1 solution-1] (measure (solve-1 parsed))
           [t2 solution-2] (measure (solve-2 parsed))]
@@ -28,7 +30,8 @@
   [["-y" "--year YEAR" "Year to run"
     :parse-fn #(Integer/parseInt %)]
    ["-d" "--day DAY" "Day to run"
-    :parse-fn #(Integer/parseInt %)]])
+    :parse-fn #(Integer/parseInt %)]
+   ["-i" "--input PATH" "Input file"]])
 
 (defn validate-args [args]
     (let [{:keys [options errors summary]} (parse-opts args cli-opts)]
@@ -48,5 +51,5 @@
   (let [{:keys [exit-message options ok?]} (validate-args args)]
     (if exit-message
       (exit (if ok? 0 1) exit-message)
-      (let [{:keys [year day]} options]
-        (run year day)))))
+      (let [{:keys [year day input]} options]
+        (run year day input)))))
